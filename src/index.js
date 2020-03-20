8e8886c5b09042ef73e33d45bf548b65557ad8f6
Likvidas <bobento@yandex.ru>
2020-03-20 12:57:07 +0300

    init: start Expression calculator

--- src/index.js
+++ src/index.js
@@ -4,7 +4,36 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+
+    let bracketOpen = 0;
+    let bracketClosed = 0;
+
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] === "(") {
+            bracketOpen++;
+        } else if (expr[i] === ")") {
+            bracketClosed++;
+        }
+    };
+
+    if (bracketOpen !== bracketClosed) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    };
+
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] === "/") {
+            if (expr[i + 2] === "0") {
+                throw new Error('TypeError: Division by zero.');
+            }
+        }
+    };
+
+    const calculate = new Function(`return ${expr.replace(/[\s]/g, '')}`);
+    const result = calculate();
+
+
+    return result;
+
 }
 
 module.exports = {
